4904a94bbeac10eb3c2f3b7c507f0ea3412a8d4a
Abhishek-Aditya-bs <abhishek.aditya10@gmail.com>
2021-11-13 07:22:12 -0800

    Convert RDD to Dataframe

--- streaming.py
+++ streaming.py
@@ -1,52 +1,40 @@
-
+from pyspark.sql import SparkSession as spark
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode,split,from_json
-from pyspark.sql.types import IntegerType, StringType, StructField, StructType
-
+from pyspark.sql.types import IntegerType, StringType, StructField, StructType,ArrayType
+from pyspark.sql import SQLContext
 import json
+import numpy as np
+def print_rdd(rdd):
+    if not rdd.isEmpty():
+        schema = StructType([StructField("image",ArrayType(ArrayType(ArrayType(IntegerType()))),True), StructField("label", ArrayType(IntegerType()), True)])
+        df = sql_context.createDataFrame(rdd, schema)
+        # for i,ele in enumerate(rdd.collect()):
+        #     print(ele)
+        df.show()
+    print("Total Batch Size of RDD :",len(rdd.collect()))
+    print("----------------------------")
 
-def parse_json(line):
-    line = json.loads(line)
-    return list(line.keys())
+sc = SparkContext("local[2]","CIFAR")
+ssc = StreamingContext(sc,1)
+sql_context = SQLContext(sc)
 
+stream = ssc.socketTextStream("localhost",6100)
 
-# spark = SparkSession.builder.appName("CIFAR").getOrCreate()
-spark = SparkContext("local[2]","CIFAR")
-ssc = StreamingContext(spark,10)
+json_stream = stream.map(lambda line: json.loads(line))   
+keys = json_stream.flatMap(lambda x:list(map(int,x.keys()))) # [0 8,1 8,2,3,4,5..31]
+                                                               
+json_stream_exploded = json_stream.flatMap(lambda x: x.values())
 
-batch = ssc.socketTextStream("localhost",6100)
+pixels = json_stream_exploded.map(lambda x: list((np.array([[v for k,v in x.items() if 'feature' in k]]).reshape(3,32,32).tolist(),[v for k,v in x.items() if 'label' in k])))
 
-batch = batch.map(lambda line: parse_json(line))
+pixels.foreachRDD(print_rdd)
 
-# batch.foreachRDD()
-batch.pprint()
-# batch.reduceByKey(lambda x: json.loads()
-# batch.saveAsTextFiles("hello")
 
 ssc.start()
 ssc.awaitTermination()
 
-# lines = spark.readStream.format("socket").option("host","localhost").option("port",6100).option().load()
 
-# lines = explode(split(lines.value,":"))
-# lines = explode(lines)
 
-# lines = lines.select().alias("words")
-# lines = lines.select(from_json(
-#     lines.value,StructType(
-#         [StructField("batch",StringType(),False),
-#          StructField("feature",StringType(),False),
-#          StructField("value",IntegerType(),False)
-#         ])).alias("json")).collect()
-
-# data = [{"0": {"a": 1}}]
-# schema = StructType([StructField("feature", StringType(),False),StructField("pixel",IntegerType(),False)])
-# df = spark.createDataFrame(data)
-
-# df = df.select(from_json(df.value,schema))
-# df = df.select(from_json(df.value, schema).alias("json")).collect()
-# df.show()
-# query = df.writeStream.outputMode("append").format("console").start()
-# query.awaitTermination()
