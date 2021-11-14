8567b381d4eedc74fce3762851371b3c05d9d54d
iVishalr <vishalramesh01@gmail.com>
2021-11-14 19:58:09 +0530

    Implement transforms on numpy matrices

--- streaming.py
+++ streaming.py
@@ -1,29 +1,33 @@
+import pyspark
 from pyspark import SparkContext
+from pyspark.sql.dataframe import DataFrame
 from pyspark.streaming import StreamingContext
 from pyspark.sql.types import IntegerType, StructField, StructType
 from pyspark.sql import SQLContext
-from pyspark.ml.feature import StandardScaler
 from sparkdl.image import imageIO
+
+from transforms import Normalize
 
 import json
 import numpy as np
 
-def Standardize(df):
-    scaler = StandardScaler(inputCol="image", outputCol="scaled_image",withStd=True, withMean=False)
-    scalerModel = scaler.fit(df)
+def standardize(df: DataFrame) -> DataFrame:
+    scaler = Normalize(mean=(0.4913997551666284, 0.48215855929893703, 0.4465309133731618), std=(0.24703225141799082, 0.24348516474564, 0.26158783926049628))
+    rdds = df.rdd.map(lambda x: [imageIO.imageStructToArray(x[0]),x[1]])
+    rdds = rdds.map(lambda x: [scaler.transform(x[0]),x[1]])
+    rdds = rdds.map(lambda x: [imageIO.imageArrayToStruct(x[0]),x[1]])
+    df = rdds.toDF(["image","label"])
+    return df
 
-    # Normalize each feature to have unit standard deviation.
-    scaledData = scalerModel.transform(df)
-    return scaledData
-
-def map_rdd_to_df(rdd):
+def map_rdd_to_df(rdd: pyspark.RDD) -> None: #Returns None as of now
     if not rdd.isEmpty():
         schema = StructType([StructField("image",imageIO.imageSchema,True), StructField("label", IntegerType(), True)])
         df = sql_context.createDataFrame(rdd, schema)
+        df = standardize(df)
         df.show()
-
-    print("Total Batch Size of RDD :",len(rdd.collect()))
-    print("----------------------------")
+        df.printSchema()
+    print("Total Batch Size of RDD Received :",len(rdd.collect()))
+    print("---------------------------------------")
 
 sc = SparkContext("local[2]","CIFAR")
 ssc = StreamingContext(sc,1)
@@ -38,7 +42,6 @@ json_stream_exploded = json_stream.flatMap(lambda x: x.values())
 
 pixels = json_stream_exploded.map(lambda x: [imageIO.imageArrayToStruct(np.array([[v for k,v in x.items() if 'feature' in k]]).reshape(32,32,3)/255.0),[v for k,v in x.items() if 'label' in k][0]])
 pixels.foreachRDD(map_rdd_to_df)
-
 
 ssc.start()
 ssc.awaitTermination()
---
+++ transforms/Normalize.py
@@ -0,0 +1,40 @@
+from typing import Tuple
+import numpy as np
+
+class Normalize:
+    """
+    Normalizes the input 3D matrix with the given mean and standard deviation. 
+    Performs per channel normalization of input matrix.
+
+    Args
+    ----
+
+    mean (Tuple) : Tuple of means of Red Channel, Green Channel, Blue Channel
+    std (Tuple) : Tuple of std of Red Channel, Green Channel, Blue Channel
+
+    Returns
+    -------
+    Normalized `numpy.ndarray` with same shape as input 
+    """
+
+    def __init__(self,mean: Tuple, std: Tuple) -> None:
+        self.mean = mean
+        self.std = std
+
+    def transform(self,matrix: np.ndarray) -> np.ndarray:
+        shape = matrix.shape
+        matrix = matrix.transpose(2,0,1)
+        r = matrix[0]
+        g = matrix[1]
+        b = matrix[2]
+        
+        r = (r-self.mean[0])/self.std[0]
+        g = (g-self.mean[1])/self.std[1]
+        b = (b-self.mean[2])/self.std[2]
+
+        matrix[0] = r
+        matrix[1] = g
+        matrix[2] = b
+        matrix = matrix.transpose(1,2,0)
+        assert matrix.shape == shape
+        return matrix
